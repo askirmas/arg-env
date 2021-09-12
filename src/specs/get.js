0ac3cba1c66add8c9172c133e47d8baae7350028
Andrii Kirmas <kirmas@phystech.edu>
2021-09-12 15:23:00 +0300

    Change envs collector to sort keys

--- src/specs/get.js
+++ src/specs/get.js
@@ -2,11 +2,24 @@ console.log(JSON.stringify(main(), null, 2))
 
 function main() {
   const {env} = process
+  , collected = {}
+  , keys = []
+
+  for (const key in env) {
+    if (!key.includes("SPEC_"))
+      continue
+
+    collected[key] = env[key]
+    keys.push(key)
+  }
+
+  const {length} = keys.sort()
   , $return = {}
 
-  for (const key in env)
-    if (key.includes("SPEC_"))
-      $return[key] = env[key]
+  for (let i = 0; i < length; i++) {
+    const key = keys[i]
+    $return[key] = collected[key]
+  }
 
   return $return
 }
