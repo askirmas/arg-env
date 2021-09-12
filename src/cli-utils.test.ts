df9ed5444d0a16c805ac20d75c4eda5a53afeefb
Andrii Kirmas <kirmas@phystech.edu>
2021-09-12 23:34:49 +0300

    Add cli utils

---
+++ src/cli-utils.test.ts
@@ -0,0 +1,96 @@
+import {
+  fromArgs,
+  fromPackageEnv
+} from "./cli-utils"
+
+describe(fromArgs.name, () => {
+  it("empty", () => expect(fromArgs([
+  ], false)).toStrictEqual([
+  ]))
+
+  it("nothing", () => expect(fromArgs([
+    'bin/node',
+    'script',
+    "--runInBand",
+    "abc"
+  ], false)).toStrictEqual([
+  ]))
+
+  it("1", () => expect(fromArgs([
+    'bin/node',
+    'script',
+    '--env-file=xxx yyy',
+    "--runInBand",
+    "--env-file=xxx yyy",
+    "abc"
+  ], false)).toStrictEqual([
+    'xxx yyy',
+    "xxx yyy",
+  ]))
+
+  it("with delete", () => {
+    const args = [
+      'bin/node',
+      'script',
+      '--env-file=xxx yyy',
+      "--runInBand",
+      "abc",
+      "--env-file=xxx"
+    ]
+    fromArgs(args, true)
+    expect(args).toStrictEqual([
+      'bin/node',
+      'script',
+      "--runInBand",
+      "abc"
+    ])
+  })
+})
+
+describe(fromPackageEnv.name, () => {
+  it("gap", () => expect(fromPackageEnv({
+    "npm_package_env_file_3": "d",
+    "npm_package_env_file_0": "a",
+    "npm_package_env_file_1": "b",
+  })).toStrictEqual([
+    "a",
+    "b",
+  ]))
+
+  it("letter", () => expect(fromPackageEnv({
+    "npm_package_env_file_0": "a",
+    "npm_package_env_file_1": "b",
+    "npm_package_env_file_a": "g",
+  })).toStrictEqual([
+    "a",
+    "b",
+  ]))
+
+  it("long list", () => expect(fromPackageEnv({
+    "npm_package_env_file_2": "c",
+    "npm_package_env_file_3": "d",
+    "npm_package_env_file_0": "a",
+    "npm_package_env_file_1": "b",
+    "npm_package_env_file_6": "g",
+    "npm_package_env_file_7": "h",
+    "npm_package_env_file_4": "e",
+    "npm_package_env_file_5": "f",
+    "npm_package_env_file_11": "m",
+    "npm_package_env_file_10": "l",
+    "npm_package_env_file_8": "",
+    "npm_package_env_file_9": "k"
+  })).toStrictEqual([
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "",
+    "k",
+    "l",
+    "m"
+  ]))
+})
---
+++ src/cli-utils.ts
@@ -0,0 +1,68 @@
+const argStart = 2
+, argPrefix = "--env-file="
+, {"length": argPrefixLength} = argPrefix
+, packagePrefix = "npm_package_env_file_"
+
+export {
+  fromArgs,
+  fromPackageEnv
+}
+
+function fromArgs(
+  argv: typeof process["argv"],
+  deleteThem: boolean
+) {
+  const {length} = argv
+  // CONSIDER `Set`
+  , collected: string[] = []
+  , indexesToDelete = deleteThem ? [] as number[] : undefined
+
+  for (let i = argStart; i < length; i++) {
+    const arg = argv[i]
+
+    if (!arg.startsWith(argPrefix))
+      continue
+
+    collected.push(arg.substr(argPrefixLength))
+    indexesToDelete?.push(i)
+  }
+
+  const lengthToDelete = indexesToDelete?.length
+
+  if (lengthToDelete) {
+    let deleteIndex = 0
+    , curIndex = indexesToDelete[0]
+
+    for (let preIndex = curIndex; preIndex < length; preIndex++) {
+      while (preIndex === indexesToDelete[deleteIndex]) {
+        preIndex++
+        deleteIndex++
+      }
+
+      argv[curIndex] = argv[preIndex]
+      curIndex++
+    }
+
+    argv.length -= lengthToDelete
+  }
+
+  return collected
+}
+
+/**
+ * @todo Consider calculation as in compose.yml
+ */
+function fromPackageEnv(env: typeof process["env"]) {
+  const collected: string[] = []
+
+  let i = 0
+  , key = `${packagePrefix}${i}`
+
+  while (key in env) {
+    collected[i] = env[key]!
+    i++
+    key = `${packagePrefix}${i}`
+  }
+
+  return collected
+}
