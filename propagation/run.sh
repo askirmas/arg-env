bc1eaf326600d6a5161815123e1a9ff4cf0aec4a
Andrii Kirmas <kirmas@phystech.edu>
2021-09-13 08:17:07 +0300

    Add more catches and overrides to propagation investigation

--- propagation/run.sh
+++ propagation/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 export ENV_EXPORT=global
 export ENV_EXPORT_2=global
+export OVERRIDE_EXPORT_ROOT=global
+export OVERRIDE_EXPORT_INLINE=global
+export OVERRIDE_EXPORT_INTERNAL=global
+export OVERRIDE_EXPORT_DOCKER=global
 # --build-arg
 docker-compose build envs_propagation
 # `docker compose` doesn't see `-e ENV_EXPORT`
-ENV_RUN=global docker-compose run --rm -e ENV_EXPORT envs_propagation
+ENV_RUN=global docker-compose run --rm \
+-e ENV_EXPORT \
+-e ENV_RUN \
+-e OVERRIDE_EXPORT_ROOT \
+-e OVERRIDE_EXPORT_INLINE \
+-e OVERRIDE_EXPORT_INTERNAL \
+-e OVERRIDE_EXPORT_DOCKER \
+envs_propagation > result.env
