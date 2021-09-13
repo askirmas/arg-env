add07124cbd92d16f41f6ff60a9525810a4abed0
Andrii Kirmas <kirmas@phystech.edu>
2021-09-14 00:05:15 +0300

    Rename propagation variables

--- propagation/get.sh
+++ propagation/get.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 export ENV_EXPORT=global
 export ENV_EXPORT_2=global
-export OVERRIDE_EXPORT_ROOT=global
-export OVERRIDE_EXPORT_INLINE=global
-export OVERRIDE_EXPORT_INTERNAL=global
-export OVERRIDE_EXPORT_DOCKER=global
+export EXPORT_OVERRIDE_ROOT=global
+export EXPORT_OVERRIDE_INLINE=global
+export EXPORT_OVERRIDE_INTERNAL=global
+export EXPORT_OVERRIDE_DOCKER=global
 # --build-arg
 docker-compose build envs_propagation
 # `docker compose` doesn't see `-e ENV_EXPORT`
 ENV_RUN=global docker-compose run --rm \
 -e ENV_EXPORT \
 -e ENV_RUN \
--e OVERRIDE_EXPORT_ROOT \
--e OVERRIDE_EXPORT_INLINE \
--e OVERRIDE_EXPORT_INTERNAL \
--e OVERRIDE_EXPORT_DOCKER \
+-e EXPORT_OVERRIDE_ROOT \
+-e EXPORT_OVERRIDE_INLINE \
+-e EXPORT_OVERRIDE_INTERNAL \
+-e EXPORT_OVERRIDE_DOCKER \
 envs_propagation > result.env
